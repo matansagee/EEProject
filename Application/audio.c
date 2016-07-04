f5078a92c3aef49d47d4e0821e4b92b0d96fe0c3
Matan Sagee <matan.sagee@ironsrc.com>
2016-07-04 23:26:41 +0300

    setting the audio to work also with udp to alsasink

--- Application/audio.c
+++ Application/audio.c
@@ -4,7 +4,9 @@
 #include<stdio.h>
 
 
-GstElement *pipeline, *alsasrc , *mulawenc ,*rtppcmupay ,*udpsink;
+//GstElement *pipeline, *alsasrc , *mulawenc ,*rtppcmupay ,*udpsink;
+GstElement *pipeline, *udpsrc , *queue ,*rtppcmudepay ,*mulawdec,*audioconvert,*autoaudiosink;
+
 GMainLoop *loop;
 
 static gboolean
@@ -47,12 +49,16 @@ void create_loop()
 
     GstBus *bus;
     guint bus_watch_id;
+    GstCaps *rx_caps;
 
     /* Initialisation */
     loop = g_main_loop_new (NULL, FALSE);
 
     /* Create gstreamer elements */
     pipeline = gst_pipeline_new ("audio-player");
+
+/*
+    //for broadcasting the mic
     alsasrc   = gst_element_factory_make ("alsasrc","alsasrc");
     mulawenc   = gst_element_factory_make ("mulawenc","mulawenc");
     rtppcmupay   = gst_element_factory_make ("rtppcmupay","rtppcmupay");
@@ -67,20 +73,49 @@ void create_loop()
     g_object_set(G_OBJECT(udpsink), "port", 5600, NULL);
 
 
-    /* we add a message handler */
+    //we add a message handler
     bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
     bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
     gst_object_unref (bus);
 
     gst_bin_add_many (GST_BIN (pipeline), alsasrc,mulawenc,rtppcmupay,udpsink, NULL);
     gst_element_link_many (alsasrc,mulawenc,rtppcmupay,udpsink, NULL);
-    gst_element_set_state (pipeline, GST_STATE_PLAYING);
+*/
+    //for receiving the mic
+    udpsrc   = gst_element_factory_make ("udpsrc","udpsrc");
+    queue   = gst_element_factory_make ("queue","queue");
+    rtppcmudepay   = gst_element_factory_make ("rtppcmudepay","rtppcmudepay");
+    mulawdec = gst_element_factory_make ("mulawdec","mulawdec");
+    audioconvert   = gst_element_factory_make ("audioconvert","audioconvert");
+    autoaudiosink   = gst_element_factory_make ("alsasink","alsasink");
 
+    rx_caps = gst_caps_new_simple("application/x-rtp",
+                                  "media", G_TYPE_STRING, "audio",
+                                  "clock-rate", G_TYPE_INT, 8000,
+                                  "encoding-name", G_TYPE_STRING, "PCMU",
+                                  NULL);
+
+    if (!pipeline || !udpsrc || !queue || !rtppcmudepay || !mulawdec || !audioconvert ||!autoaudiosink) {
+        g_printerr ("One element could not be created. Exiting.\n");
+        return;
+    }
+
+    g_object_set(G_OBJECT(udpsrc), "port", 5002, NULL);
+    g_object_set(G_OBJECT(udpsrc), "caps",rx_caps, NULL);
+    gst_caps_unref (rx_caps);
+
+    //we add a message handler
+    bus = gst_pipeline_get_bus (GST_PIPELINE (pipeline));
+    bus_watch_id = gst_bus_add_watch (bus, bus_call, loop);
+    gst_object_unref (bus);
+
+    gst_bin_add_many (GST_BIN (pipeline),udpsrc,queue,rtppcmudepay,mulawdec,audioconvert,autoaudiosink, NULL);
+    gst_element_link_many (udpsrc,queue,rtppcmudepay,mulawdec,audioconvert,autoaudiosink, NULL);
+    gst_element_set_state (pipeline, GST_STATE_PLAYING);
 
     /* Iterate */
     g_print ("Running...\n");
     g_main_loop_run (loop);
-
 
     /* Out of the main loop, clean up nicely */
     g_print ("Returned, stopping playback\n");
