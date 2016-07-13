68d670cf8695cf42f702c164b7d4f61e09fadbb3
Shani Kleinman <shani.k@outlook.co.il>
2016-07-13 11:33:41 +0000

    running in background

--- streamAudio.sh
+++ streamAudio.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-gst-launch-1.0 alsasrc device="hw:1,0" ! queue ! audioresample ! audioconvert !  mulawenc ! rtppcmupay ! udpsink host=132.66.199.244 port=5600 sync=false
+gst-launch-1.0 alsasrc device="hw:1,0" ! queue ! audioresample ! audioconvert !  mulawenc ! rtppcmupay ! udpsink host=132.66.199.244 port=5600 sync=false &
