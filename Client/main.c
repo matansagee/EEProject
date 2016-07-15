2da54eec31de75d1ccaf4b231afa8fa0fa564eb1
Matan Sagee <matan.sagee108@gmail.com>
2016-07-15 14:02:07 +0000

    work

--- Client/main.c
+++ Client/main.c
@@ -7,9 +7,12 @@
 #include <string.h>
 #include <arpa/inet.h>
 #include <wiringPi.h>
+#include <glib.h>
+#include <gtk/gtk.h>
+
 
 #define MAX_CHARACTERS_IN_STRING 256
-
+int socket_desc;
 const int device_1 = 06;
 const int device_2 = 13;
 const int device_3 = 19;
@@ -25,19 +28,29 @@ void init_gpio(){
     pinMode (device_4, OUTPUT);
     pinMode (connect_ind, OUTPUT);
 }
-
+gboolean check_network_status(gpointer null){
+    int number_of_bytes_returned = write(socket_desc , "status" , strlen("status"));
+    if (number_of_bytes_returned <=0){
+        printf("Closing session\n");
+    	close(socket_desc);
+        exit(1);
+    }
+    return TRUE;
+}
 int main(int argc, char** argv)
 {
     struct sockaddr_in client;
-    int read_size;
+    int read_size;	
     int num_bytes_send;
+	printf("hello");
+    gtk_main ();
     char* client_message = (char*) malloc(MAX_CHARACTERS_IN_STRING * sizeof(char));
-    if (client_message==NULL){
+   if (client_message==NULL){
         printf("allocating memory failed.\n");
         exit(1);
     }
     //Create socket
-    int socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
     if (socket_desc == -1)
     {
         printf("Could not create socket");
@@ -45,9 +58,9 @@ int main(int argc, char** argv)
 
     //Prepare the sockaddr_in structure
     client.sin_family = AF_INET;
-    client.sin_addr.s_addr = inet_addr("132.66.199.244");
+    client.sin_addr.s_addr = inet_addr("10.0.0.1");
     client.sin_port = htons( 5222 );
-
+    printf("trying to connect\n");
     if (connect( socket_desc, (struct sockaddr *) &client, sizeof(client)) == -1){
         perror("connection failed");
         return 1;
@@ -61,21 +74,34 @@ int main(int argc, char** argv)
 
     init_gpio();
     printf("setting GPIOs\n");
-
-    while( (read_size = recv(socket_desc , client_message , MAX_CHARACTERS_IN_STRING , 0)) > 0 )
+    g_timeout_add (1000, check_network_status,NULL);
+    	
+    while((read_size = recv(socket_desc , client_message , MAX_CHARACTERS_IN_STRING , 0)) > 0 )
     {
         char *device_num,*command;
 
         client_message[read_size] = '\0';
         printf("message: %s\n",client_message);
 
-        device_num = strtok (client_message,":"); // 1,2,3,4
+        device_num = strtok (client_message,":"); // 100,1,2,3,4,0(net status)
         command = strtok (NULL,":"); //start,stop
-        if (atoi(device_num)==0){
+  
+	if (command == NULL){
             continue;
         }
         switch(atoi(device_num))
         {
+		case 0:
+		if (atoi(command) == 1){
+			printf("application disconnected\n");
+			digitalWrite(connect_ind, LOW);
+                    	digitalWrite(device_1, LOW);
+                    	digitalWrite(device_2, LOW);
+                    	digitalWrite(device_3, LOW);
+                    	digitalWrite(device_4, LOW);
+		} else {
+		    printf("number of active connection in the server %d\n",atoi(command));
+		} break;
             case 100:
                 if (!strcmp(command,"start")){
                     digitalWrite(connect_ind, HIGH);
@@ -122,8 +148,7 @@ int main(int argc, char** argv)
         memset(client_message, 0, 2000);
     }
 
-    if (read_size<=0)
-    {
+    
         printf("session terminated\n");
         digitalWrite(connect_ind, LOW);
         digitalWrite(device_1, LOW);
@@ -131,7 +156,7 @@ int main(int argc, char** argv)
         digitalWrite(device_3, LOW);
         digitalWrite(device_4, LOW);
         printf("all gpio's set to zero\n");
-    }
+    
 
     printf("Closing session\n");
     close(socket_desc);
